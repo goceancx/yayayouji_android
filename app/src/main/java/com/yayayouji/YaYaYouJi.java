59ee20c1e0fc44c998dc4d8a76b119d530c2032d
oceancx <goceancx@gmail.com>
2015-10-24 00:50:38 +0800

    'init'

---
+++ app/src/main/java/com/yayayouji/YaYaYouJi.java
@@ -0,0 +1,13 @@
+package com.yayayouji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YaYaYouJi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yayayouji);
+    }
+}
