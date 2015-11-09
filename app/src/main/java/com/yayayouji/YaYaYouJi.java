e7a8e6a8405d787f4a939fbcbfa8fefda96555bb
wangfang <wangfang1@mioji.com>
2015-11-09 19:17:43 +0800

    add logo

--- app/src/main/java/com/yayayouji/YaYaYouJi.java
+++ app/src/main/java/com/yayayouji/YaYaYouJi.java
@@ -1,13 +1,26 @@
 package com.yayayouji;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
+/**
+ *
+ */
 public class YaYaYouJi extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.yayayouji);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
