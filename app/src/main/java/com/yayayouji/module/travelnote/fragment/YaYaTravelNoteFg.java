e5c4e68b0bf8be2ee709c40a2c0e02108d506cbb
oceancx <goceancx@gmail.com>
2016-03-22 13:28:55 +0800

    添加游记模块

--- app/src/main/java/com/yayayouji/module/travelnote/fragment/YaYaTravelNoteFg.java
+++ app/src/main/java/com/yayayouji/module/travelnote/fragment/YaYaTravelNoteFg.java
@@ -1,5 +1,6 @@
 package com.yayayouji.module.travelnote.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,8 @@ import android.view.ViewGroup;
 
 import com.yayayouji.R;
 import com.yayayouji.base.BaseFragment;
+import com.yayayouji.global.YaYaBundleKey;
+import com.yayayouji.module.travelnote.YaYaTravelNoteDetail;
 
 /**
  * 游记首页
@@ -39,12 +42,28 @@ public class YaYaTravelNoteFg extends BaseFragment {
 
     }
 
-    public static class TravelNoteAdapter extends RecyclerView.Adapter<TravelNoteAdapter.VH> {
+    private void gotoTravelNoteDetailActivity(int pos) {
+        Intent intent = new Intent(getActivity(), YaYaTravelNoteDetail.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt(YaYaBundleKey.POS, pos);
+        startActivity(intent, bundle);
+    }
+
+
+    public class TravelNoteAdapter extends RecyclerView.Adapter<TravelNoteAdapter.VH> {
 
         @Override
         public void onBindViewHolder(VH holder, int position) {
-
+            holder.itemView.setTag(position);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = (int) v.getTag();
+                    gotoTravelNoteDetailActivity(pos);
+                }
+            });
         }
+
 
         @Override
         public VH onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -58,11 +77,19 @@ public class YaYaTravelNoteFg extends BaseFragment {
         }
 
 
-        public static class VH extends RecyclerView.ViewHolder {
+        public class VH extends RecyclerView.ViewHolder {
             public VH(View itemView) {
                 super(itemView);
             }
         }
+    }
+
+    class TravelNoteItem {
+        private int view_count;
+        private String main_image_url;
+        private String title;
+        private String face_image_url;
+        private String face_icon_image_url;
     }
 
 
